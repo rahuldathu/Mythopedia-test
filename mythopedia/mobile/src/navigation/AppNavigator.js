67fa606dc74632ca71071fc15e95cbc46e416fb6
rahuldathu <rahul@gmail.com>
2025-07-03 02:45:13 +0530

    C2

--- mythopedia/mobile/src/navigation/AppNavigator.js
+++ mythopedia/mobile/src/navigation/AppNavigator.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-// import screens here
-
-const Stack = createStackNavigator();
+import { useAuth } from '../context/AuthContext';
+import AuthNavigator from './AuthNavigator';
+import MainNavigator from './MainNavigator';
 
 export default function AppNavigator() {
-  return (
-    <Stack.Navigator>
-      {/* Add screens here */}
-    </Stack.Navigator>
-  );
+  const { user, loading } = useAuth();
+
+  if (loading) return null; // or a loading spinner
+
+  return user ? <MainNavigator /> : <AuthNavigator />;
 } 
